1d5eba8232d44c039245d08c643ee1184e6cb51d
happy11end <romansenushun14@gmail.com>
2023-03-12 16:23:52 +0200

    end?

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,10 @@
 const textInput = document.getElementById("name-input");
 const output = document.getElementById("name-output");
 
+if (textInput.value !== "") {
+  output.textContent = textInput.value;
+}
+
 textInput.addEventListener("input", event => {
     if (textInput.value === "")
         output.textContent = "Anonymous"
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('input#font-size-control')
 const text = document.querySelector('span#text')
 
+text.style.fontSize = input.value + "px";
 
 input.addEventListener('input', e => {    
     text.style.fontSize = `${e.target.value}px`;
